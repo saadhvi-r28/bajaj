499a14e0827b146a840fe73cc71381f8eea8bfd7
saadhvir-28 <saadhvi.r04@gmail.com>
2025-08-29 11:47:11 +0530

    Add CORS headers to fix frontend API connection

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 const express = require('express');
 const app = express();
-app.use(express.json());
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use(express.json());
 
 // Serve static files from the public folder
 app.use(express.static('public'));
